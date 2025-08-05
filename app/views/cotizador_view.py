7a3599f9d4c4668024d8dd3a24b5d55aa5c0e7f6
DennisCeballos <86538156+DennisCeballos@users.noreply.github.com>
2025-08-04 23:52:41 -0500

    feat. Nueva vista con treeview

---
+++ app/views/cotizador_view.py
@@ -0,0 +1,175 @@
+import tkinter as tk
+from tkinter import ttk, Button, Canvas
+from models.Utils import SearchableComboBox
+
+class CotizadorView(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Gestor de Cotizaciones")
+        self.geometry("650x800")
+        self.resizable(False, False)
+        
+        self.configure(bg = "#DBDBDB")
+
+        self.canvas = Canvas(
+            bg = "#DBDBDB",
+            width = 650,
+            height = 800,
+            bd = 0,
+            highlightthickness = 0,
+            relief = "ridge"
+        )
+        self.canvas.place(x=0, y=0)
+
+        #
+        #* Seccion de busqueda de producto
+        #
+        self.canvas.create_text(
+            40.0,
+            40.0,
+            anchor="nw",
+            text="Nombre Producto",
+            fill="#000000",
+            font=("IstokWeb Regular", 12 * -1)
+        )
+        
+        self.entryBuscador = tk.Entry()
+        self.entryBuscador.place(
+            x=40.0,
+            y=60.0,
+            width=470.0,
+            height=30.0
+        )
+        
+        self.btnBuscar = Button(
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("btnBuscar clicked"),
+            relief="flat",
+            text="Buscar"
+        )
+        self.btnBuscar.place(
+            x=530.0,
+            y=60.0,
+            width=70.0,
+            height=30.0
+        )
+
+        #
+        #* Seccion de Opciones
+        #
+        self.canvas.create_text(
+            40.0,
+            110.0,
+            anchor="nw",
+            text="Opciones",
+            fill="#000000",
+            font=("IstokWeb Regular", 12 * -1)
+        )
+
+        self.tablaOpciones = ttk.Treeview(show="headings")
+        self.tablaOpciones["columns"] = ["descripcion", "precio", "origen"]
+
+        #self.tablaOpciones.column("#0")
+        self.tablaOpciones.column("descripcion", width=300, minwidth=200)
+        self.tablaOpciones.column("precio", minwidth=10, width=70-1)
+        self.tablaOpciones.column("origen", minwidth=50, width=95-1)
+
+        #self.tablaOpciones.heading("#0", text="#")
+        self.tablaOpciones.heading("descripcion", text="Nombre")
+        self.tablaOpciones.heading("precio", text="Precio S/.")
+        self.tablaOpciones.heading("origen", text="Origen")
+
+        self.tablaOpciones.place(
+            x=40, y=130,
+            width=470, height=270
+        )
+
+        self.btnAgregar = Button(
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("btnAgregar clicked"),
+            relief="flat",
+            text="Agregar"
+        )
+        self.btnAgregar.place(
+            x=530.0, y=130.0,
+            width=70.0, height=30.0
+        )
+        
+        #
+        #* Seccion de Cotizaciones
+        #
+        self.canvas.create_text(
+            40.0,
+            428.0,
+            anchor="nw",
+            text="Cotizaciones",
+            fill="#000000",
+            font=("IstokWeb Regular", 12 * -1)
+        )
+
+        self.tablaCotizaciones = ttk.Treeview(show="headings")
+        self.tablaCotizaciones["columns"] = ["descripcion", "precio", "origen"]
+
+        #self.tablaOpciones.column("#0")
+        self.tablaCotizaciones.column("descripcion", width=300, minwidth=350)
+        self.tablaCotizaciones.column("precio", minwidth=10, width=90)
+        self.tablaCotizaciones.column("origen", minwidth=50, width=120-1)
+
+        #self.tablaOpciones.heading("#0", text="#")
+        self.tablaCotizaciones.heading("descripcion", text="Nombre")
+        self.tablaCotizaciones.heading("precio", text="Precio S/.")
+        self.tablaCotizaciones.heading("origen", text="Origen")
+
+        self.tablaCotizaciones.place(
+            x=40, y=450,
+            width=560, height=200
+        )
+
+        self.btnEliminar = Button(
+            text="Eliminar",
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("btnEliminar clicked"),
+            relief="flat"
+        )
+        self.btnEliminar.place(
+            x=40.0, y=665.0,
+            width=70.0, height=30.0
+        )
+
+        #
+        #* Seccion Final de Exportar a Excel
+        #
+        self.canvas.create_text(
+            40.0,
+            725.0,
+            anchor="nw",
+            text="Exportar",
+            fill="#000000",
+            font=("IstokWeb Regular", 12 * -1)
+        )
+        self.btnExportar = Button(
+            text="Exportar Excel",
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("btnExportar Excel"),
+            relief="flat"
+        )
+        self.btnExportar.place(
+            x=40.0, y=745.0,
+            width=200.0, height=30.0
+        )
+
+    def set_buscar_callback(self, callback):
+        self.btnBuscar.config(command=callback)
+    
+    def set_agregar_callback(self, callback):
+        self.btnAgregar.config(command=callback)
+    
+    def set_eliminar_callback(self, callback):
+        self.btnEliminar.config(command=callback)
+    
+    def set_exportar_callback(self, callback):
+        self.btnExportar.config(command=callback)
